173e523fdf776c7ee8d8dd7eebbbb90388363f99
Petr Lipatov <lipatov3@gmail.com>
2023-08-30 21:57:18 +0300

    feat: static files serving

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const { connect } = require("mongoose");
 require("dotenv").config();
 //middleware
 
+app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
-app.get("/", (req, res) => {
-  res.send("Task Manager App");
-});
 
 app.use("/api/v1/tasks", tasksRoutes);
 
